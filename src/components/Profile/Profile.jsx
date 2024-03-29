b839fbbd1e1af56ef64f3ca2a968ad43c79e1bac
BillieHoup <95242599+BillieHoup@users.noreply.github.com>
2024-03-29 22:21:49 +0200

    fixed profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,15 +17,15 @@ export const Profile = ({
       </div>
 
       <ul className={css.stats}>
-        <li>
+        <li className = {css.numbers}>
           <span className={css.label}>Followers</span>
           <span className={css.quantity}>{followers}</span>
         </li>
-        <li>
+        <li className = {css.numbers}>
           <span className={css.label}>Views</span>
           <span className={css.quantity}>{views}</span>
         </li>
-        <li>
+        <li className = {css.numbers}>
           <span className={css.label}>Likes</span>
           <span className={css.quantity}>{likes}</span>
         </li>
